27dd3e707d199fbfc30d6695b2361c4754a6a468
mahardika denie <dikamahar884@gmail.com>
2020-12-27 21:21:39 +0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,63 @@
 #include <iostream>
 #include <conio.h>
-
 using namespace std;
+int val[10], val2[10];
+int n;
 
-int main(int argc, char const *argv[])
+int Sort(int a, int b)
 {
-    int value1[100];
-    int n,z;
-    cout<<endl;
-    cout<<"Sorting Array : "<<endl;
-    cout<<"Masukan Jumlah Data yang di urutkan : ";
-    cin>>n;
-    cout<<endl;
-    
-    for(int i = 0 ; i < n ; i++){
-        cout<<"Masukan Angka Ke-"<<i+1<<" : ";
-        cin>>value1[i];
-    }
+    int t;
+    t = val[b];
+    val[b] = val[a];
+    val[a] = t;
+}
 
-    for(int p = n/2 ; p > 0 ; p = p/2){
-        for(int y = p ; y < n ; y++){
-            for(int x = y - p ;x >= 0 ; x -= p ){
-                if(value1[x+p] < value1[x]){
-                    z=value1[x+p];
-                    value1[x+p] = value1[x];
-                    value1[x] = z ;
-                }
+int input()
+{
+    cout << "Masukan Jumlah Angka= ";
+    cin >> n;
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukan Angka Ke-" << i + 1 << " = ";
+        cin >> val[i];
+
+        val2[i] = val[i];
+    }
+    cout << endl;
+}
+
+void Display()
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[" << val[i] << "] ";
+    }
+    cout << endl;
+}
+
+void Eksekusi()
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = n - 1; j >= i; j--)
+        {
+            if (val[j] < val[j - 1])
+            {
+                Sort(j, j - 1);
             }
         }
+        Display();
     }
-    cout<<endl;
+    cout << endl;
+}
 
-    cout<<"Data yang Di Urutkan : "<<endl;
-
-    for(int i = 0 ; i < n ; i++){
-        cout<<value1[i]<<endl;
-    }
-    return 0;
+int main()
+{
+    cout<<"Sorting Array : "<<endl;
+    cout<<"=====================\n";
+    input();
+    Eksekusi();
 }
